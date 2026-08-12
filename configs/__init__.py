8611cf90ca00941f555ad337e518f0af7a6fa850
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-12 23:22:09 +0700

    re __all__

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv, find_dotenv
 
+__all__ = ["Config", "Prompt"]
+
 load_dotenv(find_dotenv(), override=True)
 
 from .configs import *
-
-__all__ = ["Config", "Prompt"]
