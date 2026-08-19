ab241fd1b35399b185f701492c264496c080e8ac
Lai Van Sam <samvasang1192011@gmail.com>
2026-08-19 23:00:28 +0700

    re code

--- app/core/git_automation.py
+++ app/core/git_automation.py
@@ -1,6 +1,4 @@
-import os
 import re
-import shutil
 import time
 from uuid import uuid4, UUID
 import tempfile
@@ -26,94 +24,89 @@ class GitServices:
         self.repo_url: str = (
             f"https://{self.token}@github.com/{self.name}/{self.name}.git"
         )
-        self.id_commit: UUID | int = 0
-        self.local_dir: str = tempfile.mkdtemp(prefix="monoline_")
-        self.file_path: Path = Path(self.local_dir) / "README.md"
-        self.ai_text: str = ""
 
-    def git_auto(self) -> bool:
+    def git_auto(self):
+
         try:
+            ai_text = ai.get_response()
+        except Exception as e:
+            logger.error(e)
+            ai_text = Config.RES_DEFAULT
 
-            repo = git.Repo.clone_from(self.repo_url, self.local_dir)
+        id_commit: UUID = uuid4()
 
-            if not self.file_path.is_file():
-                logger.warning(f"README.md not found at {self.file_path}")
-                return False
+        with tempfile.TemporaryDirectory(prefix="monoline_") as local_dir:
 
-            repo.git.config("user.name", self.name)
-            repo.git.config("user.email", f"{self.name}@bot.monoline.com")
+            file_path: Path = Path(local_dir) / "README.md"
 
             try:
-                self.ai_text = ai.get_response()
-            except Exception as e:
-                logger.error(e)
-                self.ai_text = Config.RES_DEFAULT
 
-            content = self.file_path.read_text(encoding="utf-8")
+                repo = git.Repo.clone_from(self.repo_url, local_dir)
 
-            new_content, change_num = re.subn(
-                r"<!--start-->.*?<!--end-->",
-                lambda _: f"<!--start-->\n{self.ai_text}\n<!--end-->",
-                content,
-                flags=re.DOTALL,
-            )
+                if not file_path.is_file():
+                    logger.warning(f"README.md not found at {file_path}")
+                    return False, None, None
 
-            if change_num == 0:
-                logger.warning(
-                    "Could not find <!--start--> or <!--end--> tags in README.md"
+                repo.git.config("user.name", self.name)
+                repo.git.config("user.email", f"{self.name}@bot.monoline.com")
+
+                content = file_path.read_text(encoding="utf-8")
+
+                new_content, change_num = re.subn(
+                    r"<!--start-->.*?<!--end-->",
+                    lambda _: f"<!--start-->\n{ai_text}\n<!--end-->",
+                    content,
+                    flags=re.DOTALL,
                 )
-                return False
 
-            self.file_path.write_text(new_content, encoding="utf-8")
+                if change_num == 0:
+                    logger.warning(
+                        "Could not find <!--start--> or <!--end--> tags in README.md"
+                    )
+                    return False, None, None
 
-            repo.index.add(["README.md"])
-            self.id_commit = uuid4()
-            repo.index.commit(f"Id: {self.id_commit}")
+                file_path.write_text(new_content, encoding="utf-8")
 
-            repo.remote(name="origin").push()
+                repo.index.add(["README.md"])
+                repo.index.commit(f"Id: {id_commit}")
 
-            logger.info(f"Successfully pushed commit ID: {self.id_commit}")
-            return True
-        except GitCommandError as git_err:
-            safe_err: str = str(git_err).replace(self.token, "ghp_github_token")
-            logger.error(
-                f"Git operation failed for commit {self.id_commit}. Details: {safe_err}"
-            )
-            return False
-        except FileNotFoundError:
-            logger.error(f"README.md not found at {self.file_path}")
-            return False
-        except Exception as e:
-            logger.error(
-                f"Unexpected error during git auto-update (ID: {self.id_commit}): {e}"
-            )
-            return False
-        finally:
-            if os.path.exists(self.local_dir):
-                shutil.rmtree(self.local_dir)
+                repo.remote(name="origin").push()
+
+                logger.info(f"Successfully pushed commit ID: {id_commit}")
+                return True, id_commit, ai_text
+            except GitCommandError as git_err:
+                safe_err: str = str(git_err).replace(self.token, "ghp_github_token")
+                logger.error(
+                    f"Git operation failed for commit {id_commit}. Details: {safe_err}"
+                )
+                return False, None, None
+            except FileNotFoundError:
+                logger.error(f"README.md not found at {file_path}")
+                return False, None, None
+            except Exception as e:
+                logger.error(
+                    f"Unexpected error during git auto-update (ID: {id_commit}): {e}"
+                )
+                return False, None, None
 
 
 class UpdateGitDB(GitServices):
-    def __init__(self):
-        super().__init__()
-        self.time: float | int = 0
-        self.success: bool = False
 
-    def _async_insert_log(self, data: dict):
+    @staticmethod
+    def _async_insert_log(data: dict):
         try:
             db.ai_res.insert_one(data)
-            logger.info(
-                f"Background database record saved for commit ID: {self.id_commit}"
-            )
+            logger.info(f"Background database record saved for commit ID: {data.get("id_commit")}")
         except Exception as e:
             logger.error(f"Error in background insert log: {e}")
 
     def main(self) -> Response:
+        status, id_commit, ai_text = self.git_auto()
+        now = time.time()
+        success = False
         try:
 
-            self.time = time.time()
-
-            if not self.git_auto():
+            if not status:
 
                 return Response(
                     f"Failed to update repository: Check server logs for details.",
@@ -123,19 +116,21 @@ class UpdateGitDB(GitServices):
 
             log_data = {
                 "username": self.name,
-                "id_commit": str(self.id_commit),
-                "message": self.ai_text,
-                "time": self.time,
+                "id_commit": str(id_commit),
+                "message": ai_text,
+                "time": now,
             }
 
-            threading.Thread(target=self._async_insert_log, args=(log_data,)).start()
+            threading.Thread(
+                target=UpdateGitDB._async_insert_log, args=(log_data,)
+            ).start()
 
-            self.success = True
+            success = True
             return Response("Done to update text", mimetype="text/plain", status=200)
         except Exception as e:
-            self.success = False
+            success = False
             logger.error(
-                f"Internal server error in main process (ID: {self.id_commit}). Details: {e}"
+                f"Internal server error in main process (ID: {id_commit}). Details: {e}"
             )
 
             return Response(
@@ -144,12 +139,12 @@ class UpdateGitDB(GitServices):
                 status=500,
             )
         finally:
-            if self.success:
+            if success:
                 db.time_limit.update_one(
                     {"username": self.name},
                     {
                         "$set": {
-                            "time_last_update": self.time,
+                            "time_last_update": now,
                             "debug": g.limit_data.get("debug", False),
                         }
                     },
